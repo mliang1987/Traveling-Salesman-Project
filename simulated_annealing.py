803777ee16026ec4315e7f5ad9c3cc9833315914
mliang1987 <mliang@gatech.edu>
2019-11-07 18:34:43 -0500

    Finished Simulated Annealing Algorithm
    Plots are generated in the \Data\Plots\SA folder.  Not very good results, especially for Roanoke.  Might I have done something wrong in the implementation?

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,11 +1,141 @@
 import util as ut
+import random
+import math
+import matplotlib.pyplot as plt
 
-class SimulatedAnnealing:
+class SimulatedAnnealing(object):
 
-    def __init__(self, name, coordinates, **kwargs):
+    def __init__(self, name, coordinates, randomSeed = 0, temperature = 0, alpha = 0.995, max_iterations=100000, stop_temp = 1e-8):
+        '''
+        Constructor for Simulated Annealing problem.
+
+        Parameters:
+        name: String for the name of the file
+        coordinates: dictionary of node IDs to coordinates.
+
+        Optional:
+        randomSeed: random seed for Python random number generator
+        alpha: temperature reduction factor
+        max_iterations: maximum number of iterations before stopping 
+        stop_temp: temperature at which to stop
+        '''
+        # Problem parameters
         self.coordinates = coordinates
         self.name = name
         self.path = []
+        self.N = len(coordinates)
+        self.nodes = list(self.coordinates.keys())
+        
+        # Seed random number generator
+        random.seed(randomSeed)
+        
+        # Annealing parameters
+        self.iteration = 1
+        self.alpha = alpha
+        self.stop = max_iterations
+        self.stop_temp = stop_temp
+        self.temperature = math.sqrt(self.N) if temperature == 0 else temperature
 
-    def get_node_coordinates(self, node_id):
-        return self.coordinates[node_id]
+        # Solutions
+        self.best_solution = None
+        self.best_fit = float("Inf")
+        self.fitness_list=[]
+
+    def random_tour(self):
+        '''
+        Random tour generated.  Identifies all nodes, then shuffles order.
+
+        Returns:
+        solution: tour path
+        fitness: tour length
+        '''
+        path = list(self.coordinates.keys())
+        random.shuffle(path)
+        fitness = ut.get_tour_distance(path, self.coordinates)
+        return path, fitness
+
+    def nearest_neighbors_tour(self):
+        '''
+        Tour generated with greedy heuristic: Picks a random starting node, then appends
+        next nearest neighbor.
+
+        Returns:
+        solution: tour path
+        fitness: tour length
+        '''
+        solution = []
+        unassigned_nodes = set(self.nodes)
+        node = random.choice(self.nodes)
+        solution.append(node)
+        unassigned_nodes.remove(node)
+        while unassigned_nodes:
+            next = min(unassigned_nodes, key=lambda x: self.distance(node, x))
+            unassigned_nodes.remove(next)
+            solution.append(next)
+            node = next
+        fitness = ut.get_tour_distance(solution, self.coordinates)
+        if fitness < self.best_fit:
+            self.best_fit = fitness
+            self.best_solution = solution
+        self.fitness_list.append(fitness)
+        return solution, fitness
+
+    def simulated_annealing(self):
+        
+        # Start with a random tour
+        self.current_solution, self.current_fit = self.random_tour()
+
+        # While annealing conditions are still met...
+        while self.temperature >= self.stop_temp and self.iteration < self.stop:
+            candidate = list(self.current_solution)
+
+            # Generate next candidate using 2-Opt
+            l = random.randint(2, self.N - 1)
+            i = random.randint(0, self.N - l)
+            candidate[i : (i + l)] = reversed(candidate[i : (i + l)])
+            
+            # Determine if the candidate is worth keeping, with either:
+            # 1. Better than currently-known best fit
+            # 2. If not, probabilistically due to annealing
+            candidate_fit = ut.get_tour_distance(candidate, self.coordinates)
+            if candidate_fit < self.current_fit:
+                self.current_fit = candidate_fit
+                self.current_solution = candidate
+            else:
+                if random.random() < math.exp(-abs(candidate_fit - self.current_fit)/self.temperature):
+                    self.current_fit = candidate_fit
+                    self.current_solution = candidate
+            if self.current_fit < self.best_fit:
+                self.best_fit = self.current_fit
+                self.best_solution = self.current_solution
+            
+            # Cooling for next iteration
+            self.temperature *= self.alpha
+            self.iteration += 1
+            self.fitness_list.append(self.current_fit)
+
+    def distance(self, n1, n2):
+        '''
+        Calculates the distances between nodes.
+
+        Parameters:
+        n1: Node 1 ID
+        n2: Node 2 ID
+
+        Returns: floating point Euclidean distance between two nodes
+        '''
+        x1,y1 = self.coordinates[n1]
+        x2,y2 = self.coordinates[n2]
+        return math.sqrt((x1-x2)**2 +(y1-y2)**2)
+
+   
+def simulated_annealing_tests():
+    all_coordinates = ut.get_all_files()
+    for city, coordinates in all_coordinates.items():
+        sa = SimulatedAnnealing(city, coordinates, alpha = 0.999)
+        sa.simulated_annealing()
+        ut.plotTSP(sa.best_solution, coordinates, title = "Simulated Annealing: "+city, save_path = "Plots/SA/"+city+".png")
+    pass
+
+if __name__ == "__main__":
+    simulated_annealing_tests()
--- util.py
+++ util.py
@@ -3,6 +3,7 @@ Utilities file for CSE6140 Traveling Salesman Problem Project
 Author: Shichao Liang
 """
 import matplotlib.pyplot as plt
+import numpy as np
 import glob
 import os
 import re
@@ -23,8 +24,71 @@ def read_tsp_file(file_name):
     coordinates = {int(coord[0]):(float(coord[1]),float(coord[2])) for coord in contents}
     return coordinates
 
-def plotTSP(path, coords):
-    pass
+def get_tour_distance(tour, coords):
+    '''
+    Calculates the tour distance, given the tour and the dictionary of coordinates
+
+    Parameters:
+    tour: list of node IDs that represent the tour.
+    coords: dictionary of coordinates for all node IDs.
+
+    Returns: An integer value for the distance of the tour.
+
+    Note: Python rounding rounds split floats to next even value.
+    '''
+    coord_array = np.asarray([coords[i] for i in tour])
+    return distance_helper(coord_array)
+
+def distance_helper(coord_array):
+    '''
+    Helper function for calculating the tour distance.  Can be used if coord_array is already
+    generated.
+    '''
+    coord_array = np.vstack((coord_array,coord_array[0]))
+    deltas = np.diff(coord_array, axis = 0)
+    segdists = np.hypot(deltas[:,0], deltas[:,1])
+    return int(np.sum(np.around(segdists, decimals = 0)))
+
+def plotTSP(tour, coords, title = None, subtitled = True, save_path = None):
+    '''
+    Generates a plot of a tour, given the tour and the dictionary of coordinates.
+
+    Parameters:
+    tour: list of node IDs that represent the tour.
+    coords: dictionary of coordinates for all node IDs.
+    
+    Optional Parameters:
+    title: string for the title of the plot. 
+    subtitled: boolean for if a subtitle for the tour should be added.
+    save_path: file path in which to save the figure.
+    '''
+    # Preprocessing tour data.
+    coord_array = np.asarray([coords[i] for i in tour])
+    distance = distance_helper(coord_array)
+    x, y = coord_array.T
+
+    # Title processing for plot
+    if title != None:
+        plt.axes([.1,.1,.8,.7])
+        plt.figtext(.5,.9,title, fontsize=18, ha='center')
+        if subtitled:
+            subtitle = "\nTour with distance {}\n".format(distance) + ', '.join([str(elem) for elem in tour])
+            plt.figtext(.5,.85,subtitle,fontsize=8,ha='center')
+
+    # Plot data
+    plt.scatter(x,y, c='r', marker = 'o')
+    arrow_scale = float(abs(max(x))-abs(min(x)))/float(60)
+    plt.arrow(x[-1], y[-1], (x[0] - x[-1]), (y[0] - y[-1]), head_width = arrow_scale,
+            color ='b', length_includes_head=True)
+    for i in range(0,len(x)-1):
+        plt.arrow(x[i], y[i], (x[i+1] - x[i]), (y[i+1] - y[i]), head_width = arrow_scale,
+                color = 'b', length_includes_head = True)
+ 
+    # Figure saving
+    if save_path != None:
+        plt.savefig(save_path)
+    plt.show()
+
 
 def get_all_files(path = 'Data/'):
     '''
