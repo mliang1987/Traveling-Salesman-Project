fdf2715d69765f751ca62729b3fd5ce053aea114
Rodrigo A. Lima <ral@gatech.edu>
2019-11-11 11:55:50 -0500

    Add BnB algorithm. Still need to make it depth-first.

---
+++ src/branch_and_bound.py
@@ -0,0 +1,127 @@
+####################################################################################################
+# CSE 6140 - Fall 2019
+#   Rodrigo Alves Lima
+#   Shichao Liang
+#   Jeevanjot Singh
+#   Kevin Tynes
+####################################################################################################
+
+from heapq import heappush, heappop
+import time
+
+from solver import Solver
+
+
+class BranchAndBoundConfiguration:
+  """A configuration of the BranchAndBoundSolver."""
+
+  def __init__(self, G, N, path):
+    """Initialize a configuration: a path in the graph.
+
+    G -- [list of list of integers] A graph represented as an adjacency matrix.
+    N -- [integer] Number of vertices in the graph.
+    path -- [list of integers] A path represented as a sequence of vertices.
+    """
+    # Set attributes.
+    self._G = G
+    self._N = N
+    self._path = path
+
+    # Prim's algorithm: Initialize the tree with any vertex that is not in the path. Then, grow the
+    # tree one edge at a time until all the vertices that are not in the path are covered.
+    root = None
+    Q = []
+    cost = {}
+    total_prim_cost = 0
+    for v in range(N):
+      if root is None and v not in path:
+        root = v
+      elif root is not None and v not in path:
+        cost[v] = G[root][v]
+        heappush(Q, (G[root][v], v))
+    tree = set([root])
+    while Q:
+      (weight, v) = heappop(Q)
+      if v not in tree:
+        total_prim_cost += weight
+        tree.add(v)
+        for u in range(N):
+          if u not in path and u not in tree and cost[u] > G[v][u]:
+            cost[u] = G[v][u]
+            heappush(Q, (G[v][u], u))
+
+    # Calculate a lower bound of any solution derived from this configuration: cost of the path +
+    # cost of the minimum spanning tree covering the vertices that are not in the path.
+    self._lower_bound = self.get_cost() + total_prim_cost
+
+  def expand(self, v):
+    """Return an expanded configuration with the specified vertex appended to the path.
+
+    v -- [integer] Vertex to be appended to the path.
+    """
+    # Check if the vertex is not in the path already, if appending it does not lead to a solution.
+    if not (len(self._path) == self._N and v == self._path[0]) and v in self._path:
+      raise ValueError
+    return BranchAndBoundConfiguration(self._G, self._N, self._path + [v])
+
+  def is_solution(self):
+    """Return True if the path is a solution. Return False, otherwise."""
+    # Only need to check the length because the configuration expansion assesses the feasibility.
+    return len(self._path) == self._N + 1
+
+  def get_cost(self):
+    """Return the cost of the path: sum of the costs of its edges."""
+    return sum([self._G[self._path[i - 1]][self._path[i]] for i in range(1, len(self._path))])
+
+  def get_lower_bound(self):
+    """Return the lower bound."""
+    return self._lower_bound
+
+  def get_path(self):
+    """Return the path."""
+    return self._path
+
+  def __lt__(self, other):
+    """Return True if this configuration is more promising than the other configuration. Return
+    False, otherwise.
+
+    other -- [Configuration] A configuration to be compared against.
+    """
+    return self._lower_bound < other._lower_bound
+
+
+class BranchAndBoundSolver(Solver):
+  """Implementation of the branch and bound algorithm using the MST-based lower bound function."""
+
+  def solve(self):
+    """Return an exact solution to the TSP problem.
+
+    TODO: Detailed explanation.
+    """
+    # Set an undefined global upper bound.
+    upper_bound = None
+
+    # Start from an empty configuration.
+    frontier = [BranchAndBoundConfiguration(self._G, self._N, [])]
+
+    # Branch and bound until the frontier set is empty.
+    while frontier:
+      # Fetch the most promising configuration.
+      config = heappop(frontier)
+      
+      # Expand configuration by appending a vertex to the path.
+      for v in range(self._N):
+        try:
+          expanded_config = config.expand(v)
+        except ValueError:
+          # Expanded configuration is not valid.
+          continue
+        if expanded_config.is_solution():
+          # Update the global upper bound, if needed.
+          upper_bound = min(upper_bound, expanded_config.get_cost()) \
+              if upper_bound else expanded_config.get_cost()
+          print("[%s] Found a solution.\n\tupper bound = %s" % (time.time(), upper_bound))
+        elif upper_bound is None or expanded_config.get_lower_bound() < upper_bound:
+          # Add to the frontier set.
+          heappush(frontier, expanded_config)
+    print("[%s] Finished solving.\n\tcost = %s" % (time.time(), upper_bound))
---
+++ src/tsp_main.py
@@ -0,0 +1,54 @@
+####################################################################################################
+# CSE 6140 - Fall 2019
+#   Rodrigo Alves Lima
+#   Shichao Liang
+#   Jeevanjot Singh
+#   Kevin Tynes
+####################################################################################################
+
+"""
+Command-line arguments:
+  -inst [string]: Filepath of a single input instance.
+  -alg [string]: Method to use.
+  -time [integer]: Cut-off time in seconds.
+  -seed [int]: Seed for random generator (optional).
+"""
+
+
+import sys
+import time
+
+from branch_and_bound import BranchAndBoundSolver
+
+
+def main():
+  # Parse command line arguments.
+  cli_args = dict((sys.argv[1 + i], sys.argv[2 + i]) for i in range(0, len(sys.argv[1:]), 2))
+  inst_arg = cli_args.get("-inst", None)
+  alg_arg = cli_args.get("-alg", None)
+  time_arg = cli_args.get("-time", None)
+  seed_arg = cli_args.get("-seed", None)
+  if not inst_arg or not alg_arg or not time_arg or \
+      (not seed_arg and alg_arg in ("Approx", "LS1", "LS2")):
+    print("Invalid arguments.")
+    sys.exit(1)
+
+  # Solve the problem instance.
+  print("[%s] Started execution.\n\tinst = %s\n\talg = %s\n\ttime = %s\n\tseed = %s" %
+      (time.time(), inst_arg, alg_arg, time_arg, seed_arg))
+  if (alg_arg == "BnB"):
+    solver = BranchAndBoundSolver(inst_arg, time_arg)
+  elif (alg_arg == "Approx"):
+    # TODO
+    raise NotImplementedError
+  elif (alg_arg == "LS1"):
+    # TODO
+    raise NotImplementedError
+  elif (alg_arg == "LS2"):
+    # TODO
+    raise NotImplementedError
+  solver.solve()
+
+
+if __name__ == "__main__":
+  main()
