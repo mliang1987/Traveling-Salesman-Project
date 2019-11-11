dcf1986ef4da4b233cf6ab3ab0c3bcce5966c677
Rodrigo A. Lima <ral@gatech.edu>
2019-11-11 14:57:08 -0500

    Fix bugs in Branch and Bound algorithm.

--- src/branch_and_bound.py
+++ src/branch_and_bound.py
@@ -27,54 +27,62 @@ class BranchAndBoundConfiguration:
     self._N = N
     self._path = path
 
-    # Prim's algorithm: Initialize the tree with any vertex that is not in the path. Then, grow the
-    # tree one edge at a time until all the vertices that are not in the path are covered.
-    root = None
-    Q = []      # Priority queue
-    cost = {}   # Cost to add a vertex to the tree
-    mst_cost = 0
-    for v in range(N):
-      if root is None and v not in path:
-        root = v
-      elif root is not None and v not in path:
-        cost[v] = G[root][v]
-        heappush(Q, (G[root][v], v))
-    tree = set([root])
-    while Q:
-      (weight, v) = heappop(Q)
-      if v not in tree:
-        mst_cost += weight
-        tree.add(v)
-        for u in range(N):
-          if u not in path and u not in tree and cost[u] > G[v][u]:
-            cost[u] = G[v][u]
-            heappush(Q, (G[v][u], u))
+    if self.is_solution():
+      self._lower_bound = None
+    else:
+      # Prim's algorithm: Initialize the tree with any vertex that is not in the path. Then, grow the
+      # tree one edge at a time until all the vertices that are not in the path are covered.
+      root = None
+      Q = []      # Priority queue
+      cost = {}   # Cost to add a vertex to the tree
+      mst_cost = 0
+      for v in range(N):
+        if root is None and v not in path:
+          root = v
+        elif root is not None and v not in path:
+          cost[v] = G[root][v]
+          heappush(Q, (G[root][v], v))
+      tree = set([root]) if root is not None else None
+      while Q:
+        (weight, v) = heappop(Q)
+        if v not in tree:
+          mst_cost += weight
+          tree.add(v)
+          for u in range(N):
+            if u not in path and u not in tree and cost[u] > G[v][u]:
+              cost[u] = G[v][u]
+              heappush(Q, (G[v][u], u))
 
-    # Calculate a lower bound of any solution derived from this configuration: cost of the path +
-    # cost of the minimum spanning tree covering the vertices that are not in the path + minimum
-    # cost of joining the path ends to that minimum spanning tree.
-    self._lower_bound = self.get_cost() + mst_cost + \
-        min([G[path[0]][v] for v in range(N) if v not in path]) if len(path) > 0 else 0 + \
-        min([G[path[-1]][v] for v in range(N) if v not in path]) if len(path) > 1 else 0
+      # Calculate a lower bound of any solution derived from this configuration:
+      #   cost of the path +
+      #   cost of the minimum spanning tree covering the vertices that are not in the path +
+      #   minimum cost of joining the path ends to that minimum spanning tree
+      self._lower_bound = self.get_path_cost() + mst_cost + \
+          min([G[path[0]][v] for v in range(N) if v not in path]) if len(path) > 0 else 0 + \
+          min([G[path[-1]][v] for v in range(N) if v not in path]) if len(path) > 1 else 0
 
   def expand(self, v):
     """Return an expanded configuration with the specified vertex appended to the path.
 
     v -- [integer] Vertex to be appended to the path.
     """
-    # Check if the vertex is not in the path already, if appending it does not lead to a solution.
-    if not (len(self._path) == self._N and v == self._path[0]) and v in self._path:
+    # Check if the vertex is not in the path already.
+    if v in self._path:
       raise ValueError
     return BranchAndBoundConfiguration(self._G, self._N, self._path + [v])
-
+    
   def is_solution(self):
     """Return True if the path is a solution. Return False, otherwise."""
     # Only need to check the length because the configuration expansion assesses the feasibility.
-    return len(self._path) == self._N + 1
+    return len(self._path) == self._N
 
-  def get_cost(self):
+  def get_path_cost(self):
     """Return the cost of the path: sum of the costs of its edges."""
     return sum([self._G[self._path[i - 1]][self._path[i]] for i in range(1, len(self._path))])
+
+  def get_cycle_cost(self):
+    """Return the cost of the cycle: sum of the costs of its edges."""
+    return self.get_path_cost() + self._G[self._path[-1]][self._path[0]]
 
   def get_lower_bound(self):
     """Return the lower bound."""
@@ -111,7 +119,7 @@ class BranchAndBoundSolver(Solver):
     while frontier:
       # Fetch the most promising configuration.
       config = heappop(frontier)
-      
+
       # Expand configuration by appending a vertex to the path.
       for v in range(self._N):
         try:
@@ -121,10 +129,10 @@ class BranchAndBoundSolver(Solver):
           continue
         if expanded_config.is_solution():
           # Update the global upper bound, if needed.
-          upper_bound = min(upper_bound, expanded_config.get_cost()) \
-              if upper_bound else expanded_config.get_cost()
+          upper_bound = min(upper_bound, expanded_config.get_cycle_cost()) \
+              if upper_bound is not None else expanded_config.get_cycle_cost()
           print("[%s] Found a solution.\n\tupper bound = %s" % (time.time(), upper_bound))
         elif upper_bound is None or expanded_config.get_lower_bound() < upper_bound:
           # Add to the frontier set.
           heappush(frontier, expanded_config)
-    print("[%s] Finished solving.\n\tcost = %s" % (time.time(), upper_bound))
+    print("[%s] Done.\n\tcost = %s" % (time.time(), upper_bound))
