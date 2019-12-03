21feb7de6673350e0c211beb38133116d5a50574
mliang1987 <mliang@gatech.edu>
2019-12-03 08:56:15 -0500

    SQD Script Done for Atlanta

--- src/sa_experiment1b.py
+++ src/sa_experiment1b.py
@@ -4,25 +4,14 @@ import time
 import math
 import pandas as pd
 import numpy as np
-
-class TimeoutException(Exception): pass
-
-def time_limit(seconds):
-    def signal_handler(signum, frame):
-        raise TimeoutException("Timed out!")
-    signal.signal(signal.SIGALRM, signal_handler)
-    signal.alarm(seconds)
-    try:
-        yield
-    finally:
-        signal.alarm(0)
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 	optimal = 2003763
 	file_path = "Data/Atlanta.tsp"
 	times = [0.01, 0.05, 0.1, 0.5, 1]
-	qualities = [0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5]
-	df = pd.DataFrame(index = qualities, columns = times)
+	qualities = [0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2]
+	df2 = pd.DataFrame(index = qualities, columns = times)
 	for max_time in times:
 		print("Running time",max_time)
 		p_values = []
@@ -36,5 +25,18 @@ if __name__ == "__main__":
 			t_count = experiment.count(True)
 			p = t_count / len(experiment)
 			p_values.append(p)
-		df[max_time] = p_values
-	print(df)
+		df2[max_time] = p_values
+	
+	plt.figure()
+	plt.gcf().subplots_adjust(bottom=0.2)
+	plt.axis([0,1.2,-0.1,1.1])
+	plt.plot(df2[0.01], color = 'b', linewidth = 1.0)
+	plt.plot(df2[0.05], color = 'g', linewidth = 1.0)
+	plt.plot(df2[0.1], color = 'r', linewidth = 1.0)
+	plt.plot(df2[0.5], color = 'b', linewidth = 1.0, linestyle = '--')
+	plt.plot(df2[1], color = 'g', linewidth = 1.0, linestyle = '--')
+	plt.legend([0.01, 0.05, 0.1, 0.5, 1])
+	plt.title("Solution Quality Distributions for Atlanta", fontsize = 10)
+	plt.ylabel("Probability(Solve)", fontsize = 8)
+	plt.xlabel("Relative Solution Quality [%]", fontsize = 8)
+	plt.savefig("sqd_ls1_atlanta.png")
