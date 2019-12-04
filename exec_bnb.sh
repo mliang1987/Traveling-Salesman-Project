bded04b52175e23eb2513b8f7c5a5e859991f8c7
Rodrigo A. Lima <ral@gatech.edu>
2019-12-03 20:47:47 -0500

    Removing unused files.

--- exec_bnb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INSTANCES="Atlanta Berlin Boston Champaign Cincinnati Denver NYC Philadelphia Roanoke SanFrancisco Toronto UKansasState UMissouri"
-
-for INSTANCE in $INSTANCES
-do
-  python3 src/tsp_main.py -inst Data/$INSTANCE.tsp -alg BnB -time 21600
-done
